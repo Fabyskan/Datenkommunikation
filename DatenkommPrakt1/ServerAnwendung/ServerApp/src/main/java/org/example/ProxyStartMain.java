5c3440d1542f8ae0a57e362def2f3081b8514acf
Fabyskan <fabian.grundner@proton.me>
2026-04-29 10:28:47 +0200

    Ich hab keine ahnung was kaputt war lol

--- DatenkommPrakt1/ServerAnwendung/ServerApp/src/main/java/org/example/ProxyStartMain.java
+++ DatenkommPrakt1/ServerAnwendung/ServerApp/src/main/java/org/example/ProxyStartMain.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.io.IOException;
 
-public class Drücke Enter zum BeendenProxyMain {
+public class ProxyStartMain {
 
     public static void main(String[] args) throws IOException {
         IO.println("Die Zwischenmaus lauscht. Drücke Enter zum Beenden.");
@@ -13,3 +13,6 @@ public class Drücke Enter zum BeendenProxyMain {
     }
 
 }
+
+
+
