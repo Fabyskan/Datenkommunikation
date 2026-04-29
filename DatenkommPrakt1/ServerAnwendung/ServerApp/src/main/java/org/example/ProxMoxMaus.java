8db393e7fb84c3b8553f3e9578e5f84a81a4ff96
Fabyskan <fabian.grundner@proton.me>
2026-04-29 15:15:05 +0200

    kleine namensänderung

--- DatenkommPrakt1/ServerAnwendung/ServerApp/src/main/java/org/example/ProxMoxMaus.java
+++ DatenkommPrakt1/ServerAnwendung/ServerApp/src/main/java/org/example/ProxMoxMaus.java
@@ -17,24 +17,24 @@ public class ProxMoxMaus {
 
         InetAddress localhost = InetAddress.getByName("localhost");
         Thread.startVirtualThread(() -> {
-            forwardTraffic(socketFromClient, localhost, SERVER_PORT, "Client -> Server");
+            weiterschicken(socketFromClient, localhost, SERVER_PORT, "Client -> Server");
         });
 
         Thread.startVirtualThread(() -> {
-            forwardTraffic(socketFromServer, localhost, CLIENT_PORT, "Server -> Client");
+            weiterschicken(socketFromServer, localhost, CLIENT_PORT, "Server -> Client");
         });
 
         System.in.read();
     }
 
-    private static void forwardTraffic(DatagramSocket listenSocket, InetAddress targetAddr, int targetPort, String direction) {
+    private static void weiterschicken(DatagramSocket listenSocket, InetAddress targetAddr, int targetPort, String direction) {
         try {
             byte[] buffer = new byte[2];
             while (true) {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 listenSocket.receive(packet);
 
-                System.out.println(direction + ": '" + (char)buffer[0] + "' mit Byte " + String.format("%02X", buffer[1]));
+                IO.println(direction + ": '" + (char)buffer[0] + "' mit Byte " + String.format("%02X", buffer[1]));
 
                 DatagramPacket forwardPacket = new DatagramPacket(
                         packet.getData(), packet.getLength(), targetAddr, targetPort
